1fb39a617ce301c7c7cdeb0f4a770434621d295b
Bertrand Caron <bertrand.fx.caron@gmail.com>
2013-11-07 22:38:40 +0100

    This may be a version accepting multiple argument functions

--- BCTrackingClass/BCTrackingClass.m
+++ BCTrackingClass/BCTrackingClass.m
@@ -23,20 +23,49 @@ void myMethodIMP(id self, SEL _cmd)
                  NSSelectorFromString([NSString stringWithFormat:@"tracked%@",NSStringFromSelector(_cmd)]));
 }
 
+void myMethodIMP1Arg(id self, SEL _cmd, id object);
+
+void myMethodIMP1Arg(id self, SEL _cmd, id object)
+{
+    //NSLog(@"_cmd : %@",NSStringFromSelector(_cmd));
+    [BCTrackingClass logCallForMethod:NSStringFromSelector(_cmd)];
+    objc_msgSend(self,
+                 NSSelectorFromString([NSString stringWithFormat:@"tracked%@",NSStringFromSelector(_cmd)]),
+                 object);
+}
+
 +(void)setUpTrackingForClass:(Class)aClass andMethodArray:(NSArray*)anArray //Array of selectorsStrings of methods to track
 {
     for (NSString* selectorString in anArray)
     {
         SEL selector = NSSelectorFromString(selectorString);
-        SEL trackedSelector = NSSelectorFromString([NSString stringWithFormat:@"tracked%@",selectorString]);
         
-        class_addMethod(aClass,
-                        trackedSelector,
-                        (IMP) myMethodIMP, "v@:");
-        
-        //Swizzle the original method with the tracked one
         Method original = class_getInstanceMethod(aClass,
                                                   selector);
+        const char* encoding = method_getTypeEncoding(original);
+        
+        //Get number of arguments
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@":" options:NSRegularExpressionCaseInsensitive error:nil];
+        NSUInteger argNumber = [regex numberOfMatchesInString:selectorString options:0 range:NSMakeRange(0, [selectorString length])];
+        
+        SEL trackedSelector = NSSelectorFromString([NSString stringWithFormat:@"tracked%@",selectorString]);
+        
+        if (argNumber==0)
+        {
+            class_addMethod(aClass,
+                        trackedSelector,
+                        (IMP) myMethodIMP, "v@:");
+        }
+        else if (argNumber==1)
+        {
+            class_addMethod(aClass,
+                            trackedSelector,
+                            (IMP) myMethodIMP1Arg, "v@:@");
+        }
+        
+        
+        //Swizzle the original method with the tracked one
+
         Method swizzled = class_getInstanceMethod(aClass,
                                                   trackedSelector);
         method_exchangeImplementations(original, swizzled);
